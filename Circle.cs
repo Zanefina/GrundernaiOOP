896bf5721f6b88623b74156d41d5a36ba230e3d0
Zanefina Qmega <zanfinaqmega@icloud.com>
2022-10-24 11:45:57 +0200

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrundernaiOOP
+{
+    class Circle
+    {
+        float _pi;
+        int _Radius;
+
+        public Circle(float Pi, int Radius)
+        {
+            this._pi = Pi;
+            this._Radius = Radius;
+        }
+        public float GetArea()
+        {
+            float area = _Radius * _Radius * _pi;
+            return area;
+        }
+        public float GetVolume()
+        {
+            float volume = (4* _pi * _Radius * _Radius * _Radius) / 3;
+            return volume;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GrundernaiOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle(3.141f, 5);
+            Console.WriteLine("Arean för cirkel med radie 5 är: {0}",c1.GetArea());
+            Console.WriteLine("Volymen för cirkel med radie 5 är: {0} ",c1.GetVolume());
+
+            Console.WriteLine("-------------------------------------------");
+
+            Circle c2 = new Circle(3.141f, 6);
+            Console.WriteLine("Arean för cirkel med radie 6 är: {0}",c2.GetArea());
+            Console.WriteLine("Volymen för cirkel med radie 6 är: {0} ",c2.GetVolume());
+
+        }
+    }
+}
